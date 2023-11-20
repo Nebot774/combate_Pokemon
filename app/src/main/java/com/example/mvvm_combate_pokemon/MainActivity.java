ce45b29460627986d6d4a74e0bd10fc0ad99d3aa
Nebot774 <adriclemenbot@gmail.com>
2023-11-20 10:14:36 +0100

    interfaz de ingresar datos del pokemon realizada

---
+++ app/src/main/java/com/example/mvvm_combate_pokemon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mvvm_combate_pokemon;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ingreso_datos_pokemon);
+        // Aquí puedes añadir más código para iniciar componentes o lógica de tu aplicación
+    }
+}
